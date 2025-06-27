b7b91c0ca19d19af37cd6c5a880397e06af6e49f
Tony Kan <tonykan@caltech.edu>
2025-06-27 14:46:50 -0700

    refactor(core): Simplify Residue struct by removing unused atom-related fields and methods

--- crates/scream-core/src/models/residue.rs
+++ crates/scream-core/src/models/residue.rs
@@ -1,53 +1,29 @@
-use super::atom::Atom;
 use std::collections::HashMap;
 
+#[derive(Debug, Clone, PartialEq)]
 pub struct Residue {
-    pub id: usize,                         // Unique identifier for the residue
+    pub id: isize,                         // Residue sequence number from source file
     pub name: String,                      // Name of the residue (e.g., "ALA", "GLY")
-    atoms: Vec<Atom>,                      // List of atoms in the residue
-    atom_map: HashMap<String, Vec<usize>>, // Map from atom name to list of atom indices
+    pub atom_indices: Vec<usize>,          // Indices of atoms belonging to this residue
+    atom_name_map: HashMap<String, usize>, // Map from atom name to its global index
 }
 
 impl Residue {
-    pub fn new(id: usize, name: &str, atoms: Vec<Atom>) -> Self {
-        let atoms_vec = atoms;
-        let mut atom_map = HashMap::new();
-        for (i, atom) in atoms_vec.iter().enumerate() {
-            atom_map
-                .entry(atom.name.clone())
-                .or_insert_with(Vec::new)
-                .push(i);
-        }
+    pub fn new(id: isize, name: &str) -> Self {
         Self {
             id,
             name: name.to_string(),
-            atoms: atoms_vec,
-            atom_map,
+            atom_indices: Vec::new(),
+            atom_name_map: HashMap::new(),
         }
     }
 
-    pub fn add_atom(&mut self, atom: Atom) {
-        let index = self.atoms.len();
-        self.atom_map
-            .entry(atom.name.clone())
-            .or_insert_with(Vec::new)
-            .push(index);
-        self.atoms.push(atom);
+    pub(crate) fn add_atom(&mut self, atom_name: &str, atom_idx: usize) {
+        self.atom_indices.push(atom_idx);
+        self.atom_name_map.insert(atom_name.to_string(), atom_idx);
     }
 
-    pub fn get_atom(&self, index: usize) -> Option<&Atom> {
-        self.atoms.get(index)
-    }
-
-    pub fn get_atoms_by_name(&self, name: &str) -> impl Iterator<Item = &Atom> {
-        self.atom_map
-            .get(name)
-            .map(|indices| indices.iter().map(move |&i| &self.atoms[i]))
-            .into_iter()
-            .flatten()
-    }
-
-    pub fn atoms(&self) -> &[Atom] {
-        &self.atoms
+    pub fn get_atom_index_by_name(&self, name: &str) -> Option<usize> {
+        self.atom_name_map.get(name).copied()
     }
 }
