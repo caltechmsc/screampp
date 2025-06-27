3fe81f13a8c5106a5722f97b47016021b0333908
Tony Kan <tianchengkan@gmail.com>
2025-06-27 08:48:58 -0700

    feat(core): Add Residue struct with methods for atom management

---
+++ crates/scream-core/src/models/residue.rs
@@ -0,0 +1,52 @@
+use super::atom::Atom;
+
+pub struct Residue {
+    pub id: usize,    // Unique identifier for the residue
+    pub name: String, // Name of the residue (e.g., "ALA", "GLY")
+    atoms: Vec<Atom>, // List of atoms in the residue
+    atom_map: std::collections::HashMap<String, Vec<usize>>, // Map from atom name to list of atom indices
+}
+
+impl Residue {
+    pub fn new(id: usize, name: &str, atoms: Vec<Atom>) -> Self {
+        let atoms_vec = atoms;
+        let mut atom_map = std::collections::HashMap::new();
+        for (i, atom) in atoms_vec.iter().enumerate() {
+            atom_map
+                .entry(atom.name.clone())
+                .or_insert_with(Vec::new)
+                .push(i);
+        }
+        Self {
+            id,
+            name: name.to_string(),
+            atoms: atoms_vec,
+            atom_map,
+        }
+    }
+
+    pub fn add_atom(&mut self, atom: Atom) {
+        let index = self.atoms.len();
+        self.atom_map
+            .entry(atom.name.clone())
+            .or_insert_with(Vec::new)
+            .push(index);
+        self.atoms.push(atom);
+    }
+
+    pub fn get_atoms_by_name<'a>(&'a self, name: &str) -> impl Iterator<Item = &'a Atom> + 'a {
+        self.atom_map
+            .get(name)
+            .map(|indices| indices.iter().map(move |&i| &self.atoms[i]))
+            .into_iter()
+            .flatten()
+    }
+
+    pub fn get_atom(&self, index: usize) -> Option<&Atom> {
+        self.atoms.get(index)
+    }
+
+    pub fn atoms(&self) -> &[Atom] {
+        &self.atoms
+    }
+}
