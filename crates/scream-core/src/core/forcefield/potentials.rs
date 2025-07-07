7a70ec11f569ce743e1a60a020776f72b457755b
Tony Kan <tonykan@caltech.edu>
2025-07-06 22:57:18 -0700

    feat(core): Implement Lennard-Jones 12-6 potential function

--- crates/scream-core/src/core/forcefield/potentials.rs
+++ crates/scream-core/src/core/forcefield/potentials.rs
@@ -1,3 +1,14 @@
 use std::f64::consts::PI;
 
-const COULOMB_CONSTANT: f64 = 332.0637; // in kcal·Å/(mol·e²)
+const COULOMB_CONSTANT: f64 = 332.0637; // In kcal·Å/(mol·e²)
+
+#[inline]
+pub fn lennard_jones_12_6(dist: f64, r_min: f64, well_depth: f64) -> f64 {
+    if dist < 1e-6 {
+        return 1e10; // Return a large positive energy for clashes
+    }
+    let rho = r_min / dist;
+    let rho6 = rho.powi(6);
+    let rho12 = rho6 * rho6;
+    well_depth * (rho12 - 2.0 * rho6)
+}
