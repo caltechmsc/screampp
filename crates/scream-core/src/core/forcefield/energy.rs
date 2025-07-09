0628bf6bcb7fd997ad347bc45f672e4acaaccfc2
Tony Kan <tonykan@caltech.edu>
2025-07-08 20:02:16 -0700

    test(core): Add unit tests for EnergyCalculator methods

--- crates/scream-core/src/core/forcefield/energy.rs
+++ crates/scream-core/src/core/forcefield/energy.rs
@@ -1,5 +1,5 @@
-use crate::core::forcefield::params::VdwParam;
-use crate::core::forcefield::potentials;
+use super::params::VdwParam;
+use super::potentials;
 use crate::core::models::atom::Atom;
 use std::f64::consts::PI;
 
@@ -88,5 +88,123 @@ impl EnergyCalculator {
         let angular_term = cos_theta.powi(4);
 
         flat_bottom_distance_energy * angular_term
+    }
+}
+
+#[cfg(test)]
+mod energy_calculator_tests {
+    use super::*;
+    use crate::core::forcefield::params::VdwParam;
+    use crate::core::models::atom::Atom;
+    use crate::core::models::ids::ResidueId;
+    use nalgebra::Point3;
+    use slotmap::KeyData;
+
+    fn residue_id_from_usize(x: usize) -> ResidueId {
+        ResidueId::from(KeyData::from_ffi(x as u64))
+    }
+
+    fn atom_with_params(
+        serial: usize,
+        pos: [f64; 3],
+        charge: f64,
+        delta: f64,
+        residue_id: ResidueId,
+    ) -> Atom {
+        let mut atom = Atom::new(serial, "X", residue_id, Point3::new(pos[0], pos[1], pos[2]));
+        atom.partial_charge = charge;
+        atom.delta = delta;
+        atom
+    }
+
+    #[test]
+    fn calculate_vdw_returns_finite_for_identical_atoms() {
+        let atom = atom_with_params(1, [0.0, 0.0, 0.0], 0.0, 0.0, residue_id_from_usize(1));
+        let vdw = VdwParam::LennardJones {
+            radius: 1.0,
+            well_depth: 1.0,
+        };
+        let energy = EnergyCalculator::calculate_vdw(&atom, &atom, &vdw, &vdw);
+        assert!(energy.is_finite());
+    }
+
+    #[test]
+    fn calculate_vdw_combines_lj_and_buckingham_correctly() {
+        let atom1 = atom_with_params(1, [0.0, 0.0, 0.0], 0.0, 0.0, residue_id_from_usize(1));
+        let atom2 = atom_with_params(2, [2.0, 0.0, 0.0], 0.0, 0.0, residue_id_from_usize(2));
+        let vdw1 = VdwParam::LennardJones {
+            radius: 1.0,
+            well_depth: 2.0,
+        };
+        let vdw2 = VdwParam::Buckingham {
+            radius: 2.0,
+            well_depth: 1.0,
+            scale: 10.0,
+        };
+        let energy = EnergyCalculator::calculate_vdw(&atom1, &atom2, &vdw1, &vdw2);
+        assert!(energy.is_finite());
+    }
+
+    #[test]
+    fn calculate_vdw_with_nonzero_delta_applies_flat_bottom() {
+        let atom1 = atom_with_params(1, [0.0, 0.0, 0.0], 0.0, 1.0, residue_id_from_usize(1));
+        let atom2 = atom_with_params(2, [0.5, 0.0, 0.0], 0.0, 1.0, residue_id_from_usize(2));
+        let vdw = VdwParam::LennardJones {
+            radius: 1.0,
+            well_depth: 1.0,
+        };
+        let energy = EnergyCalculator::calculate_vdw(&atom1, &atom2, &vdw, &vdw);
+        assert!(energy.is_finite());
+    }
+
+    #[test]
+    fn calculate_coulomb_returns_zero_for_zero_charges() {
+        let atom1 = atom_with_params(1, [0.0, 0.0, 0.0], 0.0, 0.0, residue_id_from_usize(1));
+        let atom2 = atom_with_params(2, [1.0, 0.0, 0.0], 0.0, 0.0, residue_id_from_usize(2));
+        let energy = EnergyCalculator::calculate_coulomb(&atom1, &atom2, 80.0);
+        assert_eq!(energy, 0.0);
+    }
+
+    #[test]
+    fn calculate_coulomb_returns_positive_for_like_charges() {
+        let atom1 = atom_with_params(1, [0.0, 0.0, 0.0], 1.0, 0.0, residue_id_from_usize(1));
+        let atom2 = atom_with_params(2, [1.0, 0.0, 0.0], 1.0, 0.0, residue_id_from_usize(2));
+        let energy = EnergyCalculator::calculate_coulomb(&atom1, &atom2, 80.0);
+        assert!(energy > 0.0);
+    }
+
+    #[test]
+    fn calculate_coulomb_returns_negative_for_opposite_charges() {
+        let atom1 = atom_with_params(1, [0.0, 0.0, 0.0], 1.0, 0.0, residue_id_from_usize(1));
+        let atom2 = atom_with_params(2, [1.0, 0.0, 0.0], -1.0, 0.0, residue_id_from_usize(2));
+        let energy = EnergyCalculator::calculate_coulomb(&atom1, &atom2, 80.0);
+        assert!(energy < 0.0);
+    }
+
+    #[test]
+    fn calculate_hbond_returns_zero_for_angle_below_90() {
+        let acceptor = atom_with_params(1, [0.0, 0.0, 0.0], 0.0, 0.0, residue_id_from_usize(1));
+        let hydrogen = atom_with_params(2, [1.0, 0.0, 0.0], 0.0, 0.0, residue_id_from_usize(1));
+        let donor = atom_with_params(3, [0.5, 0.5, 0.0], 0.0, 0.0, residue_id_from_usize(2));
+        let energy = EnergyCalculator::calculate_hbond(&acceptor, &hydrogen, &donor, 2.0, 1.0);
+        assert_eq!(energy, 0.0);
+    }
+
+    #[test]
+    fn calculate_hbond_returns_nonzero_for_angle_above_90() {
+        let acceptor = atom_with_params(1, [0.0, 0.0, 0.0], 0.0, 0.0, residue_id_from_usize(1));
+        let hydrogen = atom_with_params(2, [1.0, 0.0, 0.0], 0.0, 0.0, residue_id_from_usize(1));
+        let donor = atom_with_params(3, [1.0, 1.0, 0.0], 0.0, 0.0, residue_id_from_usize(2));
+        let energy = EnergyCalculator::calculate_hbond(&acceptor, &hydrogen, &donor, 2.0, 1.0);
+        assert!(energy.is_finite());
+    }
+
+    #[test]
+    fn calculate_hbond_with_nonzero_delta_applies_flat_bottom() {
+        let acceptor = atom_with_params(1, [0.0, 0.0, 0.0], 0.0, 1.0, residue_id_from_usize(1));
+        let hydrogen = atom_with_params(2, [1.0, 0.0, 0.0], 0.0, 0.0, residue_id_from_usize(1));
+        let donor = atom_with_params(3, [1.0, 1.0, 0.0], 0.0, 1.0, residue_id_from_usize(2));
+        let energy = EnergyCalculator::calculate_hbond(&acceptor, &hydrogen, &donor, 2.0, 1.0);
+        assert!(energy.is_finite());
     }
 }
