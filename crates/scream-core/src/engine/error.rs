b22616f7fee6876d38b55d4958dafc004ad74457
Tony Kan <tonykan@caltech.edu>
2025-07-10 16:48:25 -0700

    refactor(engine): Simplify ScreamError enum by removing unused variants and improving error messages

--- crates/scream-core/src/engine/error.rs
+++ crates/scream-core/src/engine/error.rs
@@ -1,9 +1,6 @@
 use crate::core::{
-    forcefield::{
-        library::LibraryLoadError, parameterization::ParameterizationError,
-        placement::PlacementLoadError,
-    },
-    io::bgf::BgfError,
+    forcefield::parameterization::ParameterizationError, forcefield::scoring::ScoringError,
+    io::bgf::BgfError, rotamers::library::LibraryLoadError,
 };
 use std::path::PathBuf;
 use thiserror::Error;
@@ -13,28 +10,41 @@ pub enum ScreamError {
     #[error("Configuration Error: {0}")]
     Config(String),
 
-    #[error("File I/O error for path '{path}': {source}")]
+    #[error("File I/O Error for path '{path}': {source}")]
     Io {
         path: PathBuf,
         #[source]
         source: std::io::Error,
     },
 
-    #[error("Failed to load BGF file: {0}")]
-    BfgLoad(#[from] BgfError),
+    #[error("BGF Parsing Error: {0}")]
+    BfgParse(#[from] BgfError),
 
-    #[error("Failed to load forcefield parameters: {0}")]
-    ForcefieldLoad(String),
+    #[error("TOML Parsing Error for path '{path}': {source}")]
+    TomlParse {
+        path: PathBuf,
+        #[source]
+        source: toml::de::Error,
+    },
 
-    #[error("Failed to load placement registry: {0}")]
-    PlacementLoad(#[from] PlacementLoadError),
+    #[error("Rotamer Library Error: {0}")]
+    RotamerLibrary(#[from] LibraryLoadError),
 
-    #[error("Failed to load rotamer library: {0}")]
-    RotamerLibraryLoad(#[from] LibraryLoadError),
-
-    #[error("System or Rotamer Parameterization Error: {0}")]
+    #[error("System Parameterization Error: {0}")]
     Parameterization(#[from] ParameterizationError),
 
+    #[error("Scoring Error: {0}")]
+    Scoring(#[from] ScoringError),
+
     #[error("Placement Algorithm Error: {0}")]
-    PlacementAlgorithm(String),
+    Placement(String),
+}
+
+impl From<(PathBuf, std::io::Error)> for ScreamError {
+    fn from(value: (PathBuf, std::io::Error)) -> Self {
+        ScreamError::Io {
+            path: value.0,
+            source: value.1,
+        }
+    }
 }
