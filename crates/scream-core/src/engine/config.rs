1b17c26f5632cff4300d1182f1fc4874bb4e6abc
Tony Kan <tonykan@caltech.edu>
2025-07-10 17:14:01 -0700

    feat(engine): Add InputFiles struct for managing input file paths

---
+++ crates/scream-core/src/engine/config.rs
@@ -0,0 +1,14 @@
+use crate::core::models::residue::ResidueType;
+use std::collections::HashMap;
+use std::path::PathBuf;
+
+#[derive(Debug, Clone)]
+pub struct InputFiles {
+    pub structure: PathBuf,
+    pub non_bonded_params: PathBuf,
+    pub topology_params: PathBuf,
+    pub placement_params: PathBuf,
+    pub charge_params: PathBuf,
+    pub delta_params: PathBuf,
+    pub rotamer_library: PathBuf,
+}
