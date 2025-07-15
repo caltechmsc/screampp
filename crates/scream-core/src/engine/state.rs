39a7dfbce172fa00be524a2b464e3916559dea4c
Tony Kan <tonykan@caltech.edu>
2025-07-15 12:19:29 -0700

    feat(engine): Implement comparison traits for Solution struct

--- crates/scream-core/src/engine/state.rs
+++ crates/scream-core/src/engine/state.rs
@@ -6,3 +6,22 @@ pub struct Solution {
     pub energy: f64,
     pub system: MolecularSystem,
 }
+
+impl PartialEq for Solution {
+    fn eq(&self, other: &Self) -> bool {
+        self.energy == other.energy
+    }
+}
+impl Eq for Solution {}
+
+impl PartialOrd for Solution {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        other.energy.partial_cmp(&self.energy)
+    }
+}
+
+impl Ord for Solution {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.partial_cmp(other).unwrap_or(std::cmp::Ordering::Equal)
+    }
+}
