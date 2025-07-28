871dad0b527df1d2aa3aaec0fe96f0dab53d8732
Tony Kan <tonykan@caltech.edu>
2025-07-27 22:11:31 -0700

    feat(core): Add bonds field to RotamerData and Rotamer structs

--- crates/scream-core/src/core/rotamers/rotamer.rs
+++ crates/scream-core/src/core/rotamers/rotamer.rs
@@ -13,9 +13,17 @@ pub struct RotamerAtomData {
 #[derive(Debug, Clone, Deserialize)]
 pub struct RotamerData {
     pub atoms: Vec<RotamerAtomData>,
+    pub bonds: Vec<(usize, usize)>,
 }
 
 #[derive(Debug, Clone)]
 pub struct Rotamer {
     pub atoms: Vec<Atom>,
+    pub bonds: Vec<(usize, usize)>,
+}
+
+impl Rotamer {
+    pub fn new(atoms: Vec<Atom>, bonds: Vec<(usize, usize)>) -> Self {
+        Self { atoms, bonds }
+    }
 }
