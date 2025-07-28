98de49d6bb691fbc134804d5024e34a06cf0339f
Tony Kan <tonykan@caltech.edu>
2025-07-28 11:08:08 -0700

    refactor(core): Change bonds field type in RotamerData from Vec<(usize, usize)> to Vec<[usize; 2]>

--- crates/scream-core/src/core/rotamers/rotamer.rs
+++ crates/scream-core/src/core/rotamers/rotamer.rs
@@ -13,17 +13,11 @@ pub struct RotamerAtomData {
 #[derive(Debug, Clone, Deserialize)]
 pub struct RotamerData {
     pub atoms: Vec<RotamerAtomData>,
-    pub bonds: Vec<(usize, usize)>,
+    pub bonds: Vec<[usize; 2]>,
 }
 
 #[derive(Debug, Clone)]
 pub struct Rotamer {
     pub atoms: Vec<Atom>,
     pub bonds: Vec<(usize, usize)>,
-}
-
-impl Rotamer {
-    pub fn new(atoms: Vec<Atom>, bonds: Vec<(usize, usize)>) -> Self {
-        Self { atoms, bonds }
-    }
 }
