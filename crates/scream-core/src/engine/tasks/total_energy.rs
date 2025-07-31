cf35c0a0f54261996ce7b9a8d1597029028818ce
Tony Kan <tonykan@caltech.edu>
2025-07-31 13:24:59 -0700

    fix(engine): Fix interaction energy calculation for improved readability and performance

--- crates/scream-core/src/engine/tasks/total_energy.rs
+++ crates/scream-core/src/engine/tasks/total_energy.rs
@@ -44,23 +44,37 @@ pub fn run(
     let scorer = Scorer::new(system, forcefield);
     let residue_pairs: Vec<_> = active_residues.iter().combinations(2).collect();
 
-    #[cfg(not(feature = "parallel"))]
-    let iterator = residue_pairs.iter();
-
-    #[cfg(feature = "parallel")]
-    let iterator = residue_pairs.par_iter();
-
-    let interaction_energy: EnergyTerm = iterator
-        .map(|pair| {
-            let res_a_id = *pair[0];
-            let res_b_id = *pair[1];
-            let atoms_a = system.residue(res_a_id).unwrap().atoms();
-            let atoms_b = system.residue(res_b_id).unwrap().atoms();
-            scorer
-                .score_interaction(atoms_a, atoms_b)
-                .map_err(EngineError::from)
-        })
-        .try_fold(EnergyTerm::default(), |acc, term| term.map(|t| acc + t))?;
+    let interaction_energy = {
+        #[cfg(not(feature = "parallel"))]
+        {
+            residue_pairs
+                .iter()
+                .map(|pair| {
+                    let res_a_id = *pair[0];
+                    let res_b_id = *pair[1];
+                    let atoms_a = system.residue(res_a_id).unwrap().atoms();
+                    let atoms_b = system.residue(res_b_id).unwrap().atoms();
+                    scorer.score_interaction(atoms_a, atoms_b)
+                })
+                .try_fold(EnergyTerm::default(), |acc, term_res| {
+                    term_res.map(|t| acc + t)
+                })?
+        }
+        #[cfg(feature = "parallel")]
+        {
+            residue_pairs
+                .par_iter()
+                .map(|pair| {
+                    let res_a_id = *pair[0];
+                    let res_b_id = *pair[1];
+                    let atoms_a = system.residue(res_a_id).unwrap().atoms();
+                    let atoms_b = system.residue(res_b_id).unwrap().atoms();
+                    scorer.score_interaction(atoms_a, atoms_b)
+                })
+                .map(|res| res.map_err(EngineError::from))
+                .try_reduce(|| EnergyTerm::default(), |a, b| Ok(a + b))?
+        }
+    };
 
     Ok(total_el_energy + interaction_energy)
 }
