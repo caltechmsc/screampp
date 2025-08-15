83a5e6058b67930c959d5dbefb6ea02b1ddc1c82
Tony Kan <tonykan@caltech.edu>
2025-08-15 04:31:24 -0700

    feat(core): Add energy field to RotamerData and Rotamer structs

--- crates/scream-core/src/core/rotamers/rotamer.rs
+++ crates/scream-core/src/core/rotamers/rotamer.rs
@@ -10,14 +10,24 @@ pub struct RotamerAtomData {
     pub force_field_type: String,
 }
 
+#[derive(Debug, Deserialize, Clone)]
+pub struct EnergyData {
+    pub bond: f64,
+    pub angle: f64,
+    pub torsion: f64,
+    pub inversion: f64,
+}
+
 #[derive(Debug, Clone, Deserialize)]
 pub struct RotamerData {
     pub atoms: Vec<RotamerAtomData>,
     pub bonds: Vec<[usize; 2]>,
+    pub energy: EnergyData,
 }
 
 #[derive(Debug, Clone)]
 pub struct Rotamer {
     pub atoms: Vec<Atom>,
     pub bonds: Vec<(usize, usize)>,
+    pub energy: EnergyData,
 }
