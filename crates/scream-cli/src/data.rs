0852a70bda5e95733a1172ee3354a4428e81d34c
Tony Kan <tonykan@caltech.edu>
2025-08-17 13:54:35 -0700

    chore(data): Update DATA_URL to point to the latest data release version

--- crates/scream-cli/src/data.rs
+++ crates/scream-cli/src/data.rs
@@ -6,7 +6,7 @@ use std::fs::{self};
 use std::path::{Path, PathBuf};
 use tracing::{debug, info, warn};
 
-const DATA_URL: &str = "https://github.com/caltechmsc/screampp/releases/download/v0.2.0-alpha.3/screampp-data-v0.2.0-alpha.3.tar.zst";
+const DATA_URL: &str = "https://github.com/caltechmsc/screampp/releases/download/v0.3.0-alpha.1/screampp-data-v0.3.0-alpha.1.tar.zst";
 
 #[derive(Debug, Clone, Copy)]
 pub enum DataProgress {
