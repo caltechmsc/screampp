7eea3260c11c79341a9c667f10296b3626d09ae0
Tony Kan <tonykan@caltech.edu>
2025-09-01 15:44:32 -0700

    feat(cli): Add FileResidueSelection enum and conversion implementation

--- crates/scream-cli/src/config/file.rs
+++ crates/scream-cli/src/config/file.rs
@@ -22,3 +22,42 @@ impl From<FileResidueSpecifier> for core_config::ResidueSpecifier {
         }
     }
 }
+
+#[derive(Deserialize, Debug, Clone)]
+#[serde(rename_all = "kebab-case", tag = "type")]
+pub enum FileResidueSelection {
+    All,
+    List {
+        #[serde(default)]
+        include: Vec<FileResidueSpecifier>,
+        #[serde(default)]
+        exclude: Vec<FileResidueSpecifier>,
+    },
+    LigandBindingSite {
+        #[serde(rename = "ligand-residue")]
+        ligand_residue: FileResidueSpecifier,
+        #[serde(rename = "radius-angstroms")]
+        radius_angstroms: f64,
+    },
+}
+
+impl From<FileResidueSelection> for core_config::ResidueSelection {
+    fn from(p: FileResidueSelection) -> Self {
+        match p {
+            FileResidueSelection::All => core_config::ResidueSelection::All,
+            FileResidueSelection::List { include, exclude } => {
+                core_config::ResidueSelection::List {
+                    include: include.into_iter().map(Into::into).collect(),
+                    exclude: exclude.into_iter().map(Into::into).collect(),
+                }
+            }
+            FileResidueSelection::LigandBindingSite {
+                ligand_residue,
+                radius_angstroms,
+            } => core_config::ResidueSelection::LigandBindingSite {
+                ligand_residue: ligand_residue.into(),
+                radius_angstroms,
+            },
+        }
+    }
+}
